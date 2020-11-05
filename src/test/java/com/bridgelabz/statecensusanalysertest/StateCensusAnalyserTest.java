e4adf8f25c1c17b2cd7e99b17495f82e9df54d3a
Raj <rajkamal11126@gmail.com>
2020-11-06 00:36:11 +0530

    Refactor : CSV file when incorrect but csv header incorrect return custom exception

--- src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
@@ -14,22 +14,63 @@ public class StateCensusAnalyserTest {
 	private String WRONG_STATE_CODE_CSV_FILE = "/StateCensusAnalyser/src/main/resources/IndiaStateCode12.csv";
 	private String INCORRECT_FILE = "/StateCensusAnalyser/gradlew.bat";
 
-	public void checkToEnsure_NumberOfRecordsMatches() throws CsvException, CensusCsvException {
-		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(STATE_CODE_CSV_FILE);
-		Assert.assertEquals(37, stateCensusAnalyser.readStateData());
-	}
-
 	@Test
-	public void givenWrongFileName_ShouldThrowNoSuchFileException() throws CsvException, CensusCsvException {
-		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(WRONG_STATE_CODE_CSV_FILE);
-		int checkNumberOfRecords = stateCensusAnalyser.readStateData();
-		Assert.assertEquals(37, stateCensusAnalyser.readStateData());
-	}
+    public void checkToEnsure_NumberOfRecordsMatches() throws CensusCsvException {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("STATE_CODE_CSV_FILE");
+        Assert.assertEquals(37, stateCensusAnalyser.readStateData());
+    }
 
-	@Test
-	public void givenWrongFileName_ShouldThrowRuntimeException() throws CsvException, CensusCsvException {
-		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("INCORRECT_FILE");
-		int checkNumberOfRecords = stateCensusAnalyser.readStateData();
-	}
+    @Test
+    public void givenWrongFileName_ShouldThrowNoSuchFileException() {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("WRONG_STATE_CODE_CSV_FILE");
+        try {
+            int value = stateCensusAnalyser.readStateData();
+            Assert.assertEquals(37, value);
+
+        } catch (CensusCsvException e) {
+            System.out.println(e.getMessage());
+            Assert.assertEquals("Please enter proper file name", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenWrongFilePath_ShouldThrowRunTimeException() {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("INCORRECT_FILE");
+            int checkNumberOfRecords = stateCensusAnalyser.readStateData();
+        } catch (CensusCsvException e) {
+            e.printStackTrace();
+            Assert.assertEquals("binding of file to failed", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenMethod_ifFoundIncorrectDelimiterPosition_ShouldReturnException() throws IOException, CensusCsvException {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("STATE_CODE_CSV_FILE");
+            int value = stateCensusAnalyser.readStateData();
+            Assert.assertEquals(37, value);
+        } catch (CensusCsvException e) {
+            System.out.println(e.getMessage());
+            Assert.assertEquals("Exception due to incorrect delimiter position", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenMethod_ifFoundNoHeader_ShouldReturnException()
+    {
+
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("STATE_CODE_CSV_FILE");
+            int value = stateCensusAnalyser.readStateData();
+            Assert.assertEquals(37, value);
+        }
+        catch (CensusCsvException e)
+        {
+            System.out.println(e.getMessage());
+            Assert.assertEquals("Exception due to Header", e.getMessage());
+        }
+
+    }
 
 }
