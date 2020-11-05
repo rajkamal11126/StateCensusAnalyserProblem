5b4cdc701c70c017cdbdcaa86c19a6647081b127
Raj <rajkamal11126@gmail.com>
2020-11-06 00:29:27 +0530

    Refactor : CSV file when currect but delimeter incorrect return custom exception

--- src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.bridgelabz.statecensusanalyser.CensusCsvException;
 import com.bridgelabz.statecensusanalyser.CsvException;
 import com.bridgelabz.statecensusanalyser.StateCensusAnalyser;
 
@@ -13,26 +14,22 @@ public class StateCensusAnalyserTest {
 	private String WRONG_STATE_CODE_CSV_FILE = "/StateCensusAnalyser/src/main/resources/IndiaStateCode12.csv";
 	private String INCORRECT_FILE = "/StateCensusAnalyser/gradlew.bat";
 
-	public void checkToEnsure_NumberOfRecordsMatches() throws CsvException {
+	public void checkToEnsure_NumberOfRecordsMatches() throws CsvException, CensusCsvException {
 		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(STATE_CODE_CSV_FILE);
 		Assert.assertEquals(37, stateCensusAnalyser.readStateData());
 	}
 
 	@Test
-	public void givenWrongFileName_ShouldThrowNoSuchFileException() throws CsvException {
+	public void givenWrongFileName_ShouldThrowNoSuchFileException() throws CsvException, CensusCsvException {
 		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(WRONG_STATE_CODE_CSV_FILE);
 		int checkNumberOfRecords = stateCensusAnalyser.readStateData();
 		Assert.assertEquals(37, stateCensusAnalyser.readStateData());
 	}
 
 	@Test
-	public void givenWrongFileName_ShouldThrowRuntimeException() throws CsvException {
-		try {
-			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("INCORRECT_FILE");
-			int checkNumberOfRecords = stateCensusAnalyser.readStateData();
-		} catch (CsvException e) {
-			Assert.assertEquals("Such type file doesn't exist", e.getMessage());
-		}
+	public void givenWrongFileName_ShouldThrowRuntimeException() throws CsvException, CensusCsvException {
+		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("INCORRECT_FILE");
+		int checkNumberOfRecords = stateCensusAnalyser.readStateData();
 	}
 
 }
