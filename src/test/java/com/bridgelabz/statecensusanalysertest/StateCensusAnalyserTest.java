6a14e1535781811893437fc58479de6edb135290
Raj <rajkamal11126@gmail.com>
2020-11-05 22:24:36 +0530

    Welcome

---
+++ src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.statecensusanalysertest;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.statecensusanalyser.StateCensusAnalyser;
+
+public class StateCensusAnalyserTest {
+	@Test
+	   public void checkToEnsure_NumberOfRecordsMatches() throws IOException {
+	        StateCensusAnalyser stateCensusAnalyser=new StateCensusAnalyser();
+	          Assert.assertEquals(37,stateCensusAnalyser.readStateData());
+	       }
+}
