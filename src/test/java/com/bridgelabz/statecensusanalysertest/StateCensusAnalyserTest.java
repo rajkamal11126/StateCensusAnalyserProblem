61a5201b6e34828b8ee7e214403987bddee35d87
Raj <rajkamal11126@gmail.com>
2020-11-06 10:34:38 +0530

    Refactor create common csv builder CSVState & CSVStateCensus

--- src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/statecensusanalysertest/StateCensusAnalyserTest.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.bridgelabz.statecensusanalyser.CSVStates;
+import com.bridgelabz.statecensusanalyser.CSVStatesCensus;
 import com.bridgelabz.statecensusanalyser.CensusCsvException;
 import com.bridgelabz.statecensusanalyser.CsvException;
 import com.bridgelabz.statecensusanalyser.StateCensusAnalyser;
@@ -19,14 +21,14 @@ public class StateCensusAnalyserTest {
 	@Test
 	public void checkToEnsure_NumberOfRecordsMatches() throws CensusCsvException {
 		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(STATE_CODE_CSV_FILE);
-		Assert.assertEquals(37, stateCensusAnalyser.readStateData());
+		Assert.assertEquals(37, stateCensusAnalyser.readStateData(CSVStates.class));
 	}
 
 	@Test
 	public void givenWrongFileName_ShouldThrowNoSuchFileException() {
 		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(WRONG_STATE_CODE_CSV_FILE);
 		try {
-			int value = stateCensusAnalyser.readStateData();
+			int value = stateCensusAnalyser.readStateData(CSVStates.class);
 			Assert.assertEquals(37, value);
 
 		} catch (CensusCsvException e) {
@@ -39,7 +41,7 @@ public class StateCensusAnalyserTest {
 	public void givenWrongFilePath_ShouldThrowRunTimeException() {
 		try {
 			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(INCORRECT_FILE);
-			int checkNumberOfRecords = stateCensusAnalyser.readStateData();
+			int checkNumberOfRecords = stateCensusAnalyser.readStateData(CSVStates.class);
 		} catch (CensusCsvException e) {
 			e.printStackTrace();
 			Assert.assertEquals("binding of file to failed", e.getMessage());
@@ -51,7 +53,7 @@ public class StateCensusAnalyserTest {
 			throws IOException, CensusCsvException {
 		try {
 			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(STATE_CODE_CSV_FILE);
-			int value = stateCensusAnalyser.readStateData();
+			int value = stateCensusAnalyser.readStateData(CSVStates.class);
 			Assert.assertEquals(37, value);
 		} catch (CensusCsvException e) {
 			System.out.println(e.getMessage());
@@ -64,7 +66,7 @@ public class StateCensusAnalyserTest {
 
 		try {
 			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(STATE_CODE_CSV_FILE);
-			int value = stateCensusAnalyser.readStateData();
+			int value = stateCensusAnalyser.readStateData(CSVStates.class);
 			Assert.assertEquals(37, value);
 		} catch (CensusCsvException e) {
 			System.out.println(e.getMessage());
@@ -76,7 +78,7 @@ public class StateCensusAnalyserTest {
 	public void givenMethod_CheckNumberOfRecodesMatchesOrNot_ForStatesCensus_ShouldReturnTrue()
 			throws IOException, CensusCsvException {
 		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(STATE_CENSUS_INFO_CSV_FILE_PATH);
-		int count = stateCensusAnalyser.readStateCensusInformation();
+		int count = stateCensusAnalyser.readStateData(CSVStatesCensus.class);
 		Assert.assertEquals(29, count);
 	}
 
@@ -84,7 +86,7 @@ public class StateCensusAnalyserTest {
 	public void givenMethod_ifFoundIncorrectName_OfStatesCensusFile_ShouldThrowException() throws IOException {
 		StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(STATE_CENSUS_INFO_CSV_FILE_PATH);
 		try {
-			int value = stateCensusAnalyser.readStateCensusInformation();
+			int value = stateCensusAnalyser.readStateData(CSVStatesCensus.class);
 			Assert.assertEquals(29, value);
 		} catch (CensusCsvException e) {
 			System.out.println(e.getMessage());
@@ -96,10 +98,10 @@ public class StateCensusAnalyserTest {
 	public void givenWrongFilePath_ShouldThrowRunTimeException_ReturnFalse() {
 		try {
 			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(INCORRECT_FILE);
-			int checkNumberOfRecords = stateCensusAnalyser.readStateCensusInformation();
+			int checkNumberOfRecords = stateCensusAnalyser.readStateData(CSVStatesCensus.class);
 		} catch (CensusCsvException e) {
 			e.printStackTrace();
-			Assert.assertEquals("binding of file to failed", e.getMessage());
+			Assert.assertEquals("Exception due to Header or mismatch data", e.getMessage());
 		}
 	}
 
@@ -108,11 +110,11 @@ public class StateCensusAnalyserTest {
 			throws IOException, CensusCsvException {
 		try {
 			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(STATE_CENSUS_INFO_CSV_FILE_PATH);
-			int value = stateCensusAnalyser.readStateCensusInformation();
+			 int value = stateCensusAnalyser.readStateData(CSVStatesCensus.class);
 			Assert.assertEquals(29, value);
 		} catch (CensusCsvException e) {
 			System.out.println(e.getMessage());
-			Assert.assertEquals("Exception due to incorrect delimiter position", e.getMessage());
+			Assert.assertEquals("Exception due to Header or mismatch data", e.getMessage());
 		}
 	}
 
@@ -121,7 +123,7 @@ public class StateCensusAnalyserTest {
 
 		try {
 			StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser(STATE_CENSUS_INFO_CSV_FILE_PATH);
-			int value = stateCensusAnalyser.readStateData();
+			int value = stateCensusAnalyser.readStateData(CSVStatesCensus.class);
 			Assert.assertEquals(29, value);
 		} catch (CensusCsvException e) {
 			System.out.println(e.getMessage());
